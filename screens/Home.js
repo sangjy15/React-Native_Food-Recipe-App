95a1b1ca13ab6aa9f4dee2548274512c85bd30b2
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2021-11-01 16:03:36 +0900

    add search bar

--- screens/Home.js
+++ screens/Home.js
@@ -2,25 +2,141 @@ import React from 'react';
 import {
     View,
     Text,
+    Image,
+    SafeAreaView,
+    TextInput,
+    FlatList,
     TouchableOpacity
 } from 'react-native';
+import { FONTS, COLORS, SIZES, icons, images, dummyData } from '../constants';
+import { CategoryCard } from '../components';
 
 const Home = ({ navigation }) => {
+
+    function renderHeader() {
+        return (
+            <View
+                style={{
+                    flexDirection:'row',
+                    marginHorizontal: SIZES.padding,
+                    alignItems: 'center',
+                    height: 80
+                }}
+            >
+                {/* Text */}
+                <View
+                    style={{
+                        flex: 1,
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: COLORS.darkGreen,
+                            ...FONTS.h2
+                        }}
+                    >Hello by Jinyoung,</Text>
+
+                    <Text
+                        style={{
+                            marginTop: 3,
+                            color: COLORS.gray,
+                            ...FONTS.body3
+                        }}
+                    >What you want to cook today?</Text>
+                </View>
+                {/* Image */}
+                <TouchableOpacity
+                    onPress={() => console.log("Profile")}
+                >
+                    <Image 
+                        source={images.profile}
+                        style={{
+                            width: 40,
+                            height: 40,
+                            borderRadius: 20
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+    function renderSearchBar() {
+        return (
+            <View
+                style={{
+                    flexDirection: 'row',
+                    height: 50,
+                    alignItems: 'center',
+                    marginHorizontal: SIZES.padding,
+                    paddingHorizontal: SIZES.radius,
+                    borderRadius: 10,
+                    backgroundColor: COLORS.lightGray
+                }}
+            >
+                <Image 
+                    source={icons.search}
+                    style={{
+                        width: 20,
+                        height: 20,
+                        tintColor: COLORS.gray
+                    }}
+                />
+
+                <TextInput 
+                    style={{
+                        marginLeft: SIZES.radius,
+                        ...FONTS.body3
+                    }}
+                    placeholderTextColor={COLORS.gray}
+                    placeholder="Search Recipes"
+                />
+            </View>
+        )
+    }
     return (
-        <View
+        <SafeAreaView
             style={{
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
+                backgroundColor: COLORS.white
             }}
         >
-            <Text>Home</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Recipe")}
-            >
-                <Text>Navigate to Recipe</Text>
-            </TouchableOpacity>
-        </View>
+            <FlatList 
+                data={dummyData.categories}
+                keyExtractor={item => `${item.id}`}
+                keyboardDismissMode="on-drag"
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={
+                    <View>
+                        {/* Header */}
+                        {renderHeader()}
+                        {/* Search Bar */}
+                        {renderSearchBar()}
+                        {/* See Recipe Card */}
+                        {/* Trending Section
+                        Category Header */}
+                    </View>
+                }
+                renderItem={({ item  }) => {
+                    return (
+                        <CategoryCard
+                            containerStyle={{
+                                marginHorizontal: SIZES.padding
+                            }}
+                            categoryItem={item}
+                            onPress={() => navigation.navigate
+                            ("Recipe", {recipe: item})}
+                        />
+                    )
+                }}
+                ListFooterComponent={
+                    <View 
+                        style={{
+                            marginBottom: 100
+                        }}
+                    />
+                }
+            />
+        </SafeAreaView>
     )
 }
 
