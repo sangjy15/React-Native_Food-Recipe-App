df06bb3087aa141baa19fe91b4519c2ae92aed64
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2021-11-03 14:45:11 +0900

    recipe page

--- screens/Recipe.js
+++ screens/Recipe.js
@@ -1,19 +1,257 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {
     View,
-    Text
+    Text,
+    Image,
+    TouchableOpacity,
+    Animated,
+    Platform
 } from 'react-native';
 
-const Recipe = () => {
+import { BlurView } from '@react-native-community/blur';
+import { SIZES, FONTS, COLORS, icons } from '../constants';
+const HEADER_HEIGHT = 350;
+
+const RecipeCreatorCardDetail = ({selectedRecipe}) => {
     return (
         <View
             style={{
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
+                flexDirection: 'row',
+                alignItems: 'center'
             }}
         >
-            <Text>Recipe</Text>
+            {/* Profile Photo */}
+            <View
+                style={{
+                    width: 40,
+                    height: 40,
+                    marginLeft: 20
+                }}
+            >
+                <Image 
+                    source={selectedRecipe?.author?.profilePic}
+                    style={{
+                        width: 40,
+                        height: 40,
+                        borderRadius: 20
+                    }}
+                />
+            </View>
+            {/* Labels */}
+            <View
+                style={{
+                    flex: 1,
+                    marginHorizontal: 20
+                }}
+            >
+                <Text style={{ color: COLORS.lightGray2, ...FONTS.body4}}>Recipe by:</Text>
+                <Text style={{ color: COLORS.white2, ...FONTS.h3}}>{selectedRecipe?.author?.name}</Text>
+            </View>
+            {/* Button */}
+            <TouchableOpacity
+                style={{
+                    width: 20,
+                    height: 30,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginRight: 20,
+                    borderRadius: 5,
+                    borderRadius: 5,
+                    borderWidth: 1,
+                    borderColor: COLORS.lightGreen1
+                }}
+                onPress={() => console.log("View Prifile")}
+            >
+                <Image
+                    source={icons.rightArrow}
+                    style={{
+                        width: 15,
+                        height: 15,
+                        tintColor: COLORS.lightGreen1
+                    }}
+                />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const RecipeCreatorCardInfo = ({selectedRecipe}) => {
+    if (Platform.OS === 'ios') {
+        return (
+            <BlurView 
+                style={{
+                    flex: 1,
+                    borderRadius: SIZES.radius
+                }}
+                blurType="dark"
+            >
+                <RecipeCreatorCardDetail 
+                    selectedRecipe={selectedRecipe}
+                />
+            </BlurView>
+        )
+    } else {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    borderRadius: SIZES.radius,
+                    backgroundColor: COLORS.transparentBlack9
+                }}
+            >
+                <RecipeCreatorCardDetail 
+                    selectedRecipe={selectedRecipe}
+                />
+            </View>
+        )
+    }
+}
+
+const Recipe = ({ navigation, route }) => {
+
+    const [selectedRecipe, setSelectedRecipe] = React.useState(null)
+
+    const scrollY = useRef(new Animated.Value(0)).current;
+    React.useEffect(() => {
+        let { recipe } = route.params
+        setSelectedRecipe(recipe)
+    }, [])
+
+    function renderRecipeCardHeader() {
+        return (
+            <View
+                style={{
+                    marginTop: -1000,
+                    paddingTop: 1000,
+                    alignItems: 'center',
+                    overflow: 'hidden'
+                }}
+            >
+                {/* Background Image */}
+                <Animated.Image 
+                    source={selectedRecipe?.image}
+                    resizeMode="contain"
+                    style={{
+                        height: HEADER_HEIGHT,
+                        width: "200%",
+                        transform: [
+                            {
+                                translateY: scrollY.interpolate({
+                                    inputRange: [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+                                    outputRange: [-HEADER_HEIGHT / 2, 0, HEADER_HEIGHT * 0.75]
+                                })
+                            },
+                            {
+                                scale: scrollY.interpolate({
+                                    inputRange: [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+                                    outputRange: [2, 1, 0.75]
+                                })
+                            }
+                        ]
+                    }}
+                />
+                {/* Recipe Creator Card */}
+                    <Animated.View
+                        style={{
+                            position: "absolute",
+                            bottom: 10,
+                            left: 30,
+                            right: 30,
+                            height: 80
+                        }}
+                    >
+                        <RecipeCreatorCardInfo 
+                            selectedRecipe={selectedRecipe}
+                        />
+                    </Animated.View>
+            </View>
+        )
+    }
+    return (
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: COLORS.white
+            }}
+        >
+            <Animated.FlatList 
+                data={selectedRecipe?.ingredients}
+                keyExtractor={item => `${item.id}`}
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={
+                    <View>
+                        {/* Header */}
+                        {renderRecipeCardHeader()}
+                        {/* Info */}
+                        {/* Ingredient Title */}
+                    </View>
+                }
+                scrollEventThrottle={16}
+                onScroll={Animated.event([
+                    { nativeEvent: {contentOffset: { y: scrollY }}}
+                ], { useNativeDriver: true })}
+                renderItem={({item}) => (
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            paddingHorizontal: 30,
+                            marginVertical: 5
+                        }}
+                    >
+                        {/* Icon */}
+                        <View
+                            style={{
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                height: 50,
+                                width: 50,
+                                borderRadius: 5,
+                                backgroundColor: COLORS.lightGray
+                            }}
+                        >
+                            <Image 
+                                source={item.icon}
+                                style={{
+                                    height: 40,
+                                    width: 40
+                                }}
+                            />
+                        </View>
+                        {/* Description */}
+                        <View
+                            style={{
+                                flex: 1,
+                                paddingHorizontal: 20,
+                                justifyContent: 'center'
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    ...FONTS.body3
+                                }}
+                            >
+                                {item.description}
+                            </Text>
+                        </View>
+                        {/* Quntity */}
+                        <View
+                            style={{
+                                alignItems: 'flex-end',
+                                justifyContent: 'center'
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    ...FONTS.body3
+                                }}
+                            >
+                                {item.quantity}
+                            </Text>
+                        </View>
+                    </View>
+                )}
+            />
         </View>
     )
 }
